d93fa7e6a96d91ae9091c9fe32d23a72dcbcbace
Saidi Qemali <78086708+saidiqemali@users.noreply.github.com>
2021-08-13 09:37:14 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace _1_Radio
+{
+    partial class Radio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(64, 103);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(400, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(293, 204);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(126, 237);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(640, 640);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(640, 640);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Radio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(745, 722);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.MaximumSize = new System.Drawing.Size(950, 950);
+            this.MinimumSize = new System.Drawing.Size(950, 950);
+            this.Name = "Radio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Radio ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using NAudio.Wave;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisioForge.Tools.TagLib.Mpeg;
+
+namespace _1_Radio
+{
+    public partial class Radio : Form
+    {
+        Dictionary<string, string> stationDict;
+        Dictionary<string, string> imgDict;
+        MediaFoundationReader mediaFoundation;
+        WaveOutEvent waveOut;
+        public Radio()
+        {
+            InitializeComponent();
+            waveOut = new WaveOutEvent();
+        }
+
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Load(imgDict[comboBox1.Text]);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Load(imgDict[comboBox1.Text]);
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+     
+
+            string url = "https://api.laut.fm/stations";
+           //  string url1 = "https://1000oldies.stream.laut.fm/1000oldies?t302=2021-08-12_11-03-07&uuid=ce304378-284e-4327-ad79-dcf80a583a23";
+            HttpClient client = new HttpClient();
+
+
+
+
+           
+
+
+
+
+            string res = await client.GetStringAsync(url);
+            var jsonDataSingle = JArray.Parse(res);
+            imgDict = new Dictionary<string, string>();
+            foreach (var station in jsonDataSingle)
+            {
+                try
+                {
+                    //comboBox1.Text = ((string)station["display_name"]);
+                    comboBox1.Items.Add(station["stream_url"]);
+                    imgDict.Add(station["stream_url"].ToString(), station["images"]["station_640x640"].ToString());
+
+
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+            comboBox1.Refresh();
+
+
+            
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (waveOut.PlaybackState == PlaybackState.Playing)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                button1.Image = Properties.Resources.playogoo;
+            }
+            else
+            {
+                if (comboBox1.Text == "")
+                {
+                    MessageBox.Show("Keine URL");
+
+                }
+                else
+                {
+                    mediaFoundation = new MediaFoundationReader(comboBox1.Text);
+                  
+                    waveOut.Init(mediaFoundation);
+                    waveOut.Play();
+                    button1.Image = Properties.Resources.stoplogo;
+                }
+            }
+
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+ 
+
+
+
+        //    OpenFileDialog open = new OpenFileDialog();
+
+        //    if (open.ShowDialog() == DialogResult.OK)
+        //    {
+        //        pictureBox1.Image = new Bitmap(open.FileName);
+        //    }
+        }
+    }
+}
